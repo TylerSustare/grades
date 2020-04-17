13b934f12570e7fa5d362db215abda9b3f3aedd6
TylerSustare <tylersustare@gmail.com>
2020-04-17 15:17:42 -0700

    lintstaged seems neat

--- web-grades/src/firebase/context.tsx
+++ web-grades/src/firebase/context.tsx
@@ -1,14 +1,7 @@
 // https://levelup.gitconnected.com/how-to-use-the-react-context-api-to-build-react-native-expo-and-firebase-apps-adda840e52b0
 import React, { createContext } from 'react';
 
-const FirebaseContext = createContext({
-
-
-
-
-
-
-});
+const FirebaseContext = createContext({});
 
 export const FirebaseProvider = FirebaseContext.Provider;
 
